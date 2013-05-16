523199f198c40337df09267582a960834e8da6a3
Blake Robertson <blake.robertson@gmail.com>
2013-05-16 02:17:37 -0400

    Fixed 2 bugs in FakeCall
    - Timezone is set to UTC now properly.
    - Create 2 contacts wasn't working.
    - Added instructions

--- manifest.php
+++ manifest.php
@@ -74,9 +74,9 @@ $manifest = array (
                    'icon' => '',
                    'is_uninstallable' => true,
                    'name' => 'Asterisk SugarCRM Connector',
-                   'published_date' => '2013-Mar-06 00:03',
+                   'published_date' => '2013-May-16 00:05',
                    'type' => 'module',
-                   'version' => '3.3.2',
+                   'version' => '3.3.3',
                    'remove_tables' => 'true',  /** This does absolutely nothing since our asterisk log table is created manually instead of as a bean **/
                    );
 
