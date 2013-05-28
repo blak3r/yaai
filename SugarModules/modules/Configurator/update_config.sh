1c7d401016c451b9f334fcb2ff48be766186e3af
Blake Robertson <blake.robertson@gmail.com>
2013-05-28 00:07:47 -0400

    Added update config script

---
+++ SugarModules/modules/Configurator/update_config.sh
@@ -0,0 +1,6 @@
+php configuratorGeneratorUtil.php > asterisk_configurator_table.tpl
+
+cat asterisk_configurator_table.tpl
+
+echo ""
+echo "^---- Inspect above, if you don't have a needed modstring defined it'll be listed at end of file"
